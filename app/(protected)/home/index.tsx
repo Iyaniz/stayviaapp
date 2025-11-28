1d3935ca7a6be130a3152f846d957e5cf2f3e720
Your Name <your.email@example.com>
2025-11-28 23:39:12 +0800

    new

--- app/(protected)/home/index.tsx
+++ app/(protected)/home/index.tsx
@@ -341,7 +341,6 @@ export default function Home() {
               </Badge>
             ))}
 
-            {/* Clear All Badge */}
             <Badge
               variant="default"
               className="flex-row items-center rounded-full bg-primary px-3 py-1"
